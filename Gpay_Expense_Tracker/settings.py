083451216197220c28aa89b1af0c1a4ed928e704
SRIJAN SARASWAT <srijansaraswat2003@gmail.com>
2025-06-08 01:22:52 +0530

    Replace crispy-bootstrap4 with crispy-bootstrap5 and update settings

--- Gpay_Expense_Tracker/settings.py
+++ Gpay_Expense_Tracker/settings.py
@@ -39,13 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Expenses',
     'rest_framework',
-    'crispy_bootstrap4',
+    'crispy_bootstrap5',
     'crispy_forms',
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
-CRISPY_TEMPLATE_PACK = "bootstrap4"
-
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
