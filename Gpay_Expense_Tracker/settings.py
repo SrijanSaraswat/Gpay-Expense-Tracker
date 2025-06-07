fb638cc02047edac9c3d9aca3e490517a28fd3f5
SRIJAN SARASWAT <srijansaraswat2003@gmail.com>
2025-06-08 01:28:29 +0530

    Added static_root

--- Gpay_Expense_Tracker/settings.py
+++ Gpay_Expense_Tracker/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
